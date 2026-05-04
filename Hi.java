d30f75784986074976bbee9f056bc7c087c2314a
sneyderandi1555-hub <sneyderandi.1555@gmail.com>
2026-05-04 09:52:13 -0500

    Fix typo in print statement in Hi.java

--- Hi.java
+++ Hi.java
@@ -2,5 +2,6 @@ public class Hi {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Andi");
+        System.out.printIn("Cambios nuevos");
     }
 }
