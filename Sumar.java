bfb9b1e25d50a048f5bca4ef0705ba18b882e91c
sneyderandi1555-hub <sneyderandi.1555@gmail.com>
2026-05-04 09:30:49 -0500

    guardando cambios

---
+++ Sumar.java
@@ -0,0 +1,8 @@
+public class Sumar {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int resultado = a + b;
+        System.out.println("La suma de " + a + " y " + b + " es: " + resultado);
+    }
+}
